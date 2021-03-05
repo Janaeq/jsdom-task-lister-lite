49f69b5b7e36ed3a6c50fed6f2db633855125e1f
Janae Quinones <janaemassa@gmail.com>
2021-03-04 20:25:44 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  console.log('The DOM is loaded');
+  let form = document.getElementById("create-task-form");
+  form.addEventListener('submit', handleSubmit);
 });
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const listNode = document.createElement('li')
+    const taskList = document.getElementById("tasks")
+    const input = event.target.querySelector("#new-task-description")
+    let inputValue = input.value
+    taskList.appendChild(listNode).innerHTML = inputValue
+    event.target.reset();
+}
